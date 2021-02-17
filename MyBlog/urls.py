ed721549d5889b36bd2f737afd45ec751ad32022
by-zarmius <by.zarmius@gmail.com>
2021-02-17 18:46:44 +0200

    start create google oath

--- MyBlog/urls.py
+++ MyBlog/urls.py
@@ -1,22 +1,9 @@
-"""MyBlog URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+
+from django.contrib.auth.views import auth_logout
 
 
 urlpatterns = [
@@ -28,7 +15,9 @@ urlpatterns = [
     path('my_rules/', include(('my_rules.urls', 'my_rules'), namespace='my_rules')),
     path('tinymce/', include('tinymce.urls')),
     path('comment/', include('comment.urls')),
+    path('', include('social_django.urls', namespace='social')),
     path('api/', include('comment.api.urls')),  # only required for API Framework
+    path('logout/', auth_logout, {'next_page': settings.LOGOUT_REDIRECT_URL}, name='logout'),
 ]
 
 if settings.DEBUG:
