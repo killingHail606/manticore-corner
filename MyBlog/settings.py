2f4d7abe9f0ec69e9dc09e9896d1f151b462c51d
by-zarmius <by.zarmius@gmail.com>
2021-02-17 16:54:22 +0200

    start create google oath

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -119,20 +119,20 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# AUTHENTICATION_BACKENDS = (
-#     'social_core.backends.google.GoogleOAuth2',
-#     'django.contrib.auth.backends.ModelBackend',
-# )
-#
-# SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '1078649821977-v5pb725fr9lcutgu13kf2nkbm0apjr94.apps.googleusercontent.com'
-# SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'BOigxxvi-XmSEBK4BbYa4iS3'
-#
-# LOGIN_URL = '/auth/login/google-oauth2/'
-#
-# LOGIN_REDIRECT_URL = '/'
-# LOGOUT_REDIRECT_URL = '/'
-#
-# SOCIAL_AUTH_URL_NAMESPACE = 'social'
+AUTHENTICATION_BACKENDS = (
+    'social_core.backends.google.GoogleOAuth2',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '1078649821977-v5pb725fr9lcutgu13kf2nkbm0apjr94.apps.googleusercontent.com'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'BOigxxvi-XmSEBK4BbYa4iS3'
+
+LOGIN_URL = '/auth/login/google-oauth2/'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+SOCIAL_AUTH_URL_NAMESPACE = 'social'
 
 
 
