a602652ed5002110803f25f0e829a83d663fe313
by-zarmius <by.zarmius@gmail.com>
2021-04-25 15:11:12 +0300

    fix settings

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = '%*_)wjp0)0%-(0$@w5uydc&-qxl7(s%@xgs3@p$i*x@e%aofnw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['193.164.150.161', '438511-cr02292.tmweb.ru', 'manticore-corner.ru', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    '193.164.150.161', '438511-cr02292.tmweb.ru', 'manticore-corner.ru',
+    'www.manticore-corner.ru', 'localhost', '127.0.0.1'
+]
 
 
 # Application definition
